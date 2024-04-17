6a7076f9ce0507dc37d04b029bd8b0fd57ba6923
w <zealmakes@gmail.com>
2024-04-18 00:39:57 +0800

    asdf

--- technicals and arts/beepboop.js
+++ technicals and arts/beepboop.js
@@ -1,8 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+$(document).not("#aboutboxbutton").click(function() {
+    $('#aboutbox').hide();
+});
