8b616527bf97b55babd2a3db316c6747559de6a6
w <zealmakes@gmail.com>
2024-04-23 00:28:00 +0800

    added stuff
    just added some new things, the examples and how to page should be easy to do but I can't do it right now because of a headache. shit hurts

--- technicals and arts/beepboop.js
+++ technicals and arts/beepboop.js
@@ -1,42 +1,41 @@
 $(document).ready(function(){
     $("#aboutboxbutton").focusin(function(){
-      $(this).css("background-color", "#FFFFCC");
+      $("#changeAbout").attr("src", "technicals and arts/activeaboutButton.png");
       $("#aboutbox").css("visibility", "visible");
     });
     $("#aboutboxbutton").focusout(function(){
-      $(this).css("background-color", "#FFFFFF");
+      $("#changeAbout").attr("src", "technicals and arts/aboutButton.png");
       $("#aboutbox").css("visibility", "hidden");
     });
 
     $("#toolsboxbutton").focusin(function(){
-        $(this).css("background-color", "#FFFFCC");
+      $("#changeTools").attr("src", "technicals and arts/activetoolsButton.png");
         $("#toolsbox").css("visibility", "visible");
       });
 
     $("#toolsboxbutton").focusout(function(){
-        $(this).css("background-color", "#FFFFFF");
+      $("#changeTools").attr("src", "technicals and arts/toolsButton.png");
         $("#toolsbox").css("visibility", "hidden");
     });
 
     $("#limitationsboxbutton").focusin(function(){
-        $(this).css("background-color", "#FFFFCC");
+      $("#changeLimitations").attr("src", "technicals and arts/activelimitationsButton.png");
         $("#limitationsbox").css("visibility", "visible");
       });
 
     $("#limitationsboxbutton").focusout(function(){
-        $(this).css("background-color", "#FFFFFF");
+      $("#changeLimitations").attr("src", "technicals and arts/limitationsButton.png");
         $("#limitationsbox").css("visibility", "hidden");
     });
 
     $("#coolboxbutton").focusin(function(){
-        $(this).css("background-color", "#FFFFCC");
+      $("#changeCool").attr("src", "technicals and arts/activecoolthingsButton.png");
         $("#coolbox").css("visibility", "visible");
       });
 
     $("#coolboxbutton").focusout(function(){
-        $(this).css("background-color", "#FFFFFF");
+      $("#changeCool").attr("src", "technicals and arts/coolthingsButton.png");
         $("#coolbox").css("visibility", "hidden");
     });
-
 
 }); 
